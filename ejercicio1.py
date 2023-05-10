d6bd5717d35308c9808490a4b30df1b12a51d326
Jojo3235 <joralmand@gmail.com>
2023-05-10 23:12:28 +0200

    1 2 3 y 4

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,27 +1,24 @@
-import heapq
-
 class HuffmanNode:
-    def __init__(self, char, frecuencia, izquierda=None, derecha=None):
+    def __init__(self, char, freq, izd=None, dcha=None):
         self.char = char
-        self.frecuencia = frecuencia
-        self.izquierda = izquierda
-        self.derecha = derecha
+        self.freq = freq
+        self.izd = izd
+        self.dcha = dcha
 
     def __lt__(self, other):
         return self.freq < other.freq
 
-    @staticmethod
     def construir_arbol_huffman(frecuencias):
         nodos = []
         for caracter, frecuencia in frecuencias.items():
             nodos.append(HuffmanNode(caracter, frecuencia))
         while len(nodos) > 1:
-            nodos.sort(key=lambda x: x.frecuencia)
-            nodo_izquierda = nodos.pop(0)
-            nodo_derecha = nodos.pop(0)
-            nodo_padre = HuffmanNode(None, nodo_izquierda.frecuencia + nodo_derecha.frecuencia)
-            nodo_padre.izquierda = nodo_izquierda
-            nodo_padre.derecha = nodo_derecha
+            nodos.sort(key=lambda x: x.freq)
+            nodo_izd = nodos.pop(0)
+            nodo_dcha = nodos.pop(0)
+            nodo_padre = HuffmanNode(None, nodo_izd.freq + nodo_dcha.freq)
+            nodo_padre.izd = nodo_izd
+            nodo_padre.dcha = nodo_dcha
             nodos.append(nodo_padre)
         return nodos[0]
 
@@ -30,21 +27,30 @@ def decode_string(root, s):
     curr = root
     for bit in s:
         if bit == '0':
-            curr = curr.izquierda
+            curr = curr.izd
         else:
-            curr = curr.derecha
+            curr = curr.dcha
         if curr.char is not None:
             result.append(curr.char)
             curr = root
     return ''.join(result)
 
-# Example usage
-freq_map_1 = {'A': 0.098, 'B': 0.017, 'C': 0.035, 'D': 0.026, 'E': 0.125, 'G': 0.026, 'I': 0.053, 'L': 0.053, 'M': 0.026, 'N': 0.053, 'O': 0.062, 'P': 0.035, 'Q': 0.008, 'R': 0.08, 'S': 0.035, 'T': 0.026, 'U': 0.035, 'V': 0.017, ' ': 0.15, ',': 0.017}
-root_1 = HuffmanNode.construir_arbol_huffman(freq_map_1)
-encoded_1 = "10001011101011000010111010001110000011011000000111100111101001011000011010011100110100010111010111111101000011110011111100111101000110001100000010110101111011111110111010110110111001110110111100111111100101001010010100000101101011000101100110100011100100101100001100100011010110101011111111111011011101110010000100101011000111111100010001110110011001011010001101111101011010001101110000000111001001010100011111100001100101101011100110011110100011000110000001011010111110011100"
-decoded_1 = decode_string(root_1, encoded_1)
-print("Mensaje 1: ", decoded_1)
-root_2 = HuffmanNode.construir_arbol_huffman(freq_map_1)
-encoded_2 = "0110101011011100101000111101011100110111010110110100001000111010100101111010011111110111001010001111010111001101110101100001100010011010001110010010001100010110011001110010010000111101111010"
-decoded_2 = decode_string(root_2, encoded_2)
-print("Mensaje 2: ", decoded_2)
+def espacio_ocupado(frecuencias, encoded):
+    return len(frecuencias) * 8 + len(encoded)
+
+def main():
+    freq_map_1 = {'A': 0.098, 'B': 0.017, 'C': 0.035, 'D': 0.026, 'E': 0.125, 'G': 0.026, 'I': 0.053, 'L': 0.053, 'M': 0.026, 'N': 0.053, 'O': 0.062, 'P': 0.035, 'Q': 0.008, 'R': 0.08, 'S': 0.035, 'T': 0.026, 'U': 0.035, 'V': 0.017, ' ': 0.15, ',': 0.017}
+    root_1 = HuffmanNode.construir_arbol_huffman(freq_map_1)
+    encoded_1 = "10001011101011000010111010001110000011011000000111100111101001011000011010011100110100010111010111111101000011110011111100111101000110001100000010110101111011111110111010110110111001110110111100111111100101001010010100000101101011000101100110100011100100101100001100100011010110101011111111111011011101110010000100101011000111111100010001110110011001011010001101111101011010001101110000000111001001010100011111100001100101101011100110011110100011000110000001011010111110011100"
+    decoded_1 = decode_string(root_1, encoded_1)
+    print("Mensaje 1: ", decoded_1)
+    print("Espacio ocupado: ", espacio_ocupado(freq_map_1, encoded_1), "bits")
+
+    root_2 = HuffmanNode.construir_arbol_huffman(freq_map_1)
+    encoded_2 = "0110101011011100101000111101011100110111010110110100001000111010100101111010011111110111001010001111010111001101110101100001100010011010001110010010001100010110011001110010010000111101111010"
+    decoded_2 = decode_string(root_2, encoded_2)
+    print("Mensaje 2: ", decoded_2)
+    print("Espacio ocupado: ", espacio_ocupado(freq_map_1, encoded_2), "bits")
+          
+if __name__ == "__main__":
+    main()
--- ejercicio3.py
+++ ejercicio3.py
@@ -1,66 +1,87 @@
-# Ejercicio 3
-
-# Se requiere implementar una red de ferrocarriles compuesta de estaciones de trenes y cambios de agujas (o desvíos).
-# Contemplar las siguientes consideraciones:
-# cada vértice del grafo no dirigido tendrá un tipo (estación o desvió) y su nombre,
-# en el caso de los desvíos el nombre es un número –estos estarán numerados de manera consecutiva-;
-# cada desvío puede tener múltiples puntos de entrada y salida;
-# se deben cargar seis estaciones de trenes y doce cambios de agujas;
-# cada cambio de aguja debe tener al menos cuatro salida o vértices adyacentes;
-# y cada estación como máximo dos salidas o llegadas y no puede haber dos estaciones co- nectadas directamente;
-# encontrar el camino más corto desde:
-# la estación King's Cross hasta la estación Waterloo,
-# la estación Victoria Train Station hasta la estación Liverpool Street Station,
-# la estación St. Pancras hasta la estación King's Cross;
-
 import heapq
 
-# Definir el grafo
-graph = {
-    'King\'s Cross': {'Euston': 1, 'Farringdon': 2},
-    'Euston': {'King\'s Cross': 1, 'Camden Town': 1},
-    'Farringdon': {'King\'s Cross': 2, 'Liverpool Street Station': 2, 'Barbican': 1},
-    'Camden Town': {'Euston': 1, 'Chalk Farm': 1},
-    'Chalk Farm': {'Camden Town': 1, 'Hampstead Heath': 2},
-    'Hampstead Heath': {'Chalk Farm': 2, 'Golders Green': 3},
-    'Golders Green': {'Hampstead Heath': 3, 'Finchley Central': 4},
-    'Finchley Central': {'Golders Green': 4, 'High Barnet': 5},
-    'High Barnet': {'Finchley Central': 5},
-    'Barbican': {'Farringdon': 1, 'Moorgate': 1},
-    'Moorgate': {'Barbican': 1, 'Liverpool Street Station': 1},
-    'Liverpool Street Station': {'Farringdon': 2, 'Moorgate': 1, 'Whitechapel': 2, 'Victoria Train Station': 4},
-    'Whitechapel': {'Liverpool Street Station': 2, 'Shadwell': 1},
-    'Shadwell': {'Whitechapel': 1, 'Bank': 3},
-    'Bank': {'Shadwell': 3, 'Waterloo': 4},
-    'Waterloo': {'Bank': 4},
-    'Victoria Train Station': {'Westminster': 2, 'Pimlico': 1, 'Liverpool Street Station': 4},
-    'Westminster': {'Victoria Train Station': 2, 'Waterloo': 2},
-    'Pimlico': {'Victoria Train Station': 1, 'Vauxhall': 2},
-    'Vauxhall': {'Pimlico': 2, 'Clapham Junction': 4},
-    'Clapham Junction': {'Vauxhall': 4, 'Balham': 3, 'Wimbledon': 6},
-    'Balham': {'Clapham Junction': 3, 'Tooting Bec': 2},
-    'Tooting Bec': {'Balham': 2, 'Tooting Broadway': 1},
-    'Tooting Broadway': {'Tooting Bec': 1, 'Colliers Wood': 1},
-    'Colliers Wood': {'Tooting Broadway': 1, 'South Wimbledon': 1},
-    'South Wimbledon': {'Colliers Wood': 1, 'Wimbledon': 1},
-    'Wimbledon': {'South Wimbledon': 1, 'Clapham Junction': 6},
-    'St. Pancras': {'King\'s Cross': 1}
+# Construimos el grafo como un diccionario de listas
+grafo = {
+    'King\'s Cross': ['St. Pancras', 'Waterloo', '1', '2', '6'],
+    'St. Pancras': ['King\'s Cross', '1', '2', '11'],
+    'Victoria Train Station': ['3', '4', '5', '6'],
+    'Waterloo': ['King\'s Cross', '5', '6', '7', '8', '9', '10', '11', '12'],
+    'Liverpool Street Station': ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+    '1': ['King\'s Cross', '2', '3', '4', '5', '6'],
+    '2': ['King\'s Cross', '1', '3', '4', '5', '6'],
+    '3': ['1', '2', 'Liverpool Street Station', 'Victoria Train Station'],
+    '4': ['1', '2', 'Liverpool Street Station', 'Victoria Train Station'],
+    '5': ['1', '2', 'Liverpool Street Station', 'Waterloo', 'Victoria Train Station'],
+    '6': ['1', '2', 'Liverpool Street Station', 'Waterloo', 'King\'s Cross'],
+    '7': ['1', '2', 'Liverpool Street Station', 'Waterloo', '3'],
+    '8': ['1', '2', 'Liverpool Street Station', 'Waterloo', '4'],
+    '9': ['1', '2', 'Liverpool Street Station', 'Waterloo', '5'],
+    '10': ['1', '2', 'Liverpool Street Station', 'Waterloo', '2'],
+    '11': ['1', '2', 'Liverpool Street Station', 'Waterloo', '6'],
+    '12': ['1', '2', 'Liverpool Street Station', 'Waterloo', '11'],
 }
 
-def dijkstra(graph, start, end):
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-    queue = [(0, start)]
-    while len(queue) > 0:
-        current_distance, current_node = heapq.heappop(queue)
-        if current_distance > distances[current_node]:
-            continue
-        for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(queue, (distance, neighbor))
-    return distances[end]
+# Definimos una función para encontrar el camino más corto entre dos estaciones utilizando Dijkstra
+def dijkstra_camino_mas_corto(origen, destino):
+    # Verificamos si las estaciones existen en el grafo
+    if origen not in grafo or destino not in grafo:
+        return []
 
-print('La distancia más corta entre King\'s Cross y Waterloo es: ', dijkstra(graph, 'King\'s Cross', 'Waterloo'))
-print('La distancia más corta entre King\'s Cross y St.Pancras es: ', dijkstra(graph, 'St. Pancras', 'King\'s Cross' ))
+    # Definimos una función de costo para las aristas
+    def costo(a, b):
+        return 1  # Todas las aristas tienen el mismo costo
+
+    # Utilizamos el algoritmo de Dijkstra para encontrar el camino más corto
+    heap = [(0, origen)]
+    visitados = set()
+    padres = {}
+    distancias = {origen: 0}
+    while heap:
+        (dist, v) = heapq.heappop(heap)
+        if v in visitados:
+            continue
+        visitados.add(v)
+        if v == destino:
+            # Si llegamos al destino, construimos el camino de regreso
+            camino = [destino]
+            while camino[-1] != origen:
+                camino.append(padres[camino[-1]])
+            camino.reverse()
+            return camino
+        for w in grafo[v]:
+            if w in visitados:
+                continue
+            if w not in distancias or distancias[w] > dist + costo(v, w):
+                distancias[w] = dist + costo(v, w)
+                padres[w] = v
+                heapq.heappush(heap, (distancias[w], w))
+
+    # Si no se encontró un camino, devolvemos una lista vacía
+    return []
+
+def main():
+    # Definimos las estaciones de origen y destino
+    origen = 'King\'s Cross'
+    destino = 'Waterloo'
+
+    origen_2 = 'Victoria Train Station'
+    destino_2 = 'Liverpool Street Station'
+
+    origen_3 = 'St. Pancras'
+    destino_3 = 'King\'s Cross'
+
+    # Encontramos el camino más corto entre las estaciones utilizando Dijkstra
+    camino = dijkstra_camino_mas_corto(origen, destino)
+    print('El camino más corto entre {} y {} es: {}'.format(origen, destino, camino))
+    print("Distancia: ", len(camino) - 1, " estaciones")
+
+    camino_2 = dijkstra_camino_mas_corto(origen_2, destino_2)
+    print('El camino más corto entre {} y {} es: {}'.format(origen_2, destino_2, camino_2))
+    print("Distancia: ", len(camino_2) - 1, " estaciones")
+
+    camino_3 = dijkstra_camino_mas_corto(origen_3, destino_3)
+    print('El camino más corto entre {} y {} es: {}'.format(origen_3, destino_3, camino_3))
+    print("Distancia: ", len(camino_3) - 1, " estaciones")
+
+if __name__ == '__main__':
+    main()
